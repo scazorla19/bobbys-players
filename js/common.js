e5fdea44112aa813d34ca7f54f66d075c402b4db
Bobby Peck <soldier.moth@gmail.com>
2019-08-15 23:48:36 -0700

    Cleanup cruft, add local storage persistence

--- js/common.js
+++ js/common.js
@@ -8,3 +8,28 @@ const mergeAndRemoveNull = (...args) => {
     });
     return result;
 };
+
+function persistAll() {
+    document.querySelectorAll('[data-persist]').forEach((elem) => {
+        localStorage.setItem(elem.dataset.persist, elem.value);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Restore from cache
+    document.querySelectorAll('[data-persist]').forEach((elem) => {
+        const saved = localStorage.getItem(elem.dataset.persist);
+        if (!!saved) {
+            elem.value = saved;
+        }
+    });
+    // Bind Example Manifests
+    const manifest_targets = document.querySelectorAll('[data-manifest-target]');
+    document.querySelectorAll('[data-example-manifest]').forEach((btn) => {
+        btn.onclick = () => {
+            manifest_targets.forEach((target) => {
+                target.value = btn.dataset.exampleManifest;
+            });
+        };
+    });
+});
